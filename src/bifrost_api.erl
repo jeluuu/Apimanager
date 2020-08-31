92aa460571fbe474f0b72e99590bebbf1fc58950
Chaitanya Chalasani <chaitanya@arknode.io>
2020-08-31 16:59:46 +0200

    Sock client implementation and other code cosmetic changes

--- src/bifrost_api.erl
+++ src/bifrost_api.erl
@@ -42,13 +42,6 @@
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link(Registration, Callback, Arguments, Options) ->
   gen_server:start_link(Registration, ?MODULE, [Callback|Arguments], Options).
 
@@ -62,17 +55,6 @@ initialize(Server) ->
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 % Init for gen_server behaviour
 init([Callback|Arguments]) ->
   process_flag(trap_exit, true),
@@ -92,34 +74,10 @@ init(Req, State) ->
   ReqFinalState = handle_api(Req, State),
   {ok, ReqFinalState, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
   Reply = ok,
   {reply, Reply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_cast(initialize, #{callback := Callback
                           ,init_arguments := Arguments
                           ,routes := Routes} = State) ->
@@ -133,16 +91,6 @@ handle_cast(initialize, #{callback := Callback
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_info(Info, #{callback := Callback} = State) ->
   case erlang:function_exported(Callback, handle_info, 1) of
     true -> Callback:handle_info(Info);
@@ -150,29 +98,10 @@ handle_info(Info, #{callback := Callback} = State) ->
   end,
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 terminate(_Reason, #{routes := Routes}) ->
   [ unpublish_route(Route) || Route <- Routes ],
   ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
--- src/bifrost_sock_client.erl
+++ src/bifrost_sock_client.erl
@@ -10,9 +10,18 @@
 
 -behaviour(gen_server).
 
+-callback init(Args :: list()) ->
+  {'ok', State :: map()} | {'stop', Reason :: term()}.
+-callback handle_info(Info :: term(), CState :: map()) ->
+  {'ok', State :: map()} | {'stop', Reason :: term(), State :: map()}.
+-callback terminate(Reason :: term(), CState :: map()) -> 'ok'.
+
+-optional_callbacks([terminate/2]).
+
 %% API
 -export([start_link/4
-        ,start_link/3]).
+        ,start_link/3
+        ,send/2]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -26,110 +35,113 @@
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link(Registration, Callback, Arguments, Options) ->
   gen_server:start_link(Registration, ?MODULE, [Callback|Arguments], Options).
 
 start_link(Callback, Arguments, Options) ->
   gen_server:start_link(?MODULE, [Callback|Arguments], Options).
 
+send(Client, Message) ->
+  MessageJson = jiffy:encode(Message),
+  gen_server:call(Client, {send, MessageJson}).
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
-init([]) ->
-  {ok, #{}}.
+init([Callback|Arguments]) ->
+  case Callback:init(Arguments) of
+    {ok, #{host := Host, port := Port, state := CState} = State} ->
+      Resource = maps:get(resource, State, "/"),
+      Headers = maps:get(headers, State, []),
+      {ok, #{status => init
+            ,callback => Callback
+            ,host => Host
+            ,port => Port
+            ,resource => Resource
+            ,headers => Headers
+            ,cstate => CState}, 0};
+    {stop, Reason} ->
+      {stop, Reason}
+  end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
   Reply = ok,
   {reply, Reply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
+handle_info(timeout, #{host := Host
+                      ,port := Port
+                      ,resource := Resource
+                      ,headers := Headers} = State) ->
+  case gun:open(Host, Port) of
+    {ok, ConnPid} ->
+      case gun:await_up(ConnPid) of
+        {ok, _Protocol} ->
+          gun:ws_upgrade(ConnPid, Resource, Headers),
+          {noreply, State#{conn_pid => ConnPid, status => init_upgrade}};
+        Error ->
+          gun:close(ConnPid),
+          Error
+      end;
+    Error ->
+      Error
+  end;
+handle_info({gun_upgrade, ConnPid, StreamRef, Protocols, Headers}
+           ,#{conn_pid := ConnPid} = State) ->
+  erlang:send_after(5000, self(), ping),
+  lager:info("Upgraded to websocket ~p", [{ConnPid, StreamRef, Protocols, Headers}]),
+  {noreply, State#{status => connected}};
+handle_info(ping, #{conn_pid := ConnPid, status := connected} = State) ->
+  erlang:send_after(5000, self(), ping),
+  gun:ws_send(ConnPid, {text, ""}),
+  {noreply, State};
+handle_info({gun_response, ConnPid, _, _, Status, Headers}, #{conn_pid := ConnPid} = State) ->
+  gun:close(ConnPid),
+  {stop, {upgrade_failed, Status, Headers}, State};
+handle_info({gun_error, ConnPid, StreamRef, Reason}, #{conn_pid := ConnPid} = State) ->
+  gen:close(ConnPid),
+  {stop, {upgrade_failed, StreamRef, Reason}, State};
+handle_info({gun_ws, ConnPid, _StreamnRef, {text, <<>>}}, #{conn_pid := ConnPid} = State) ->
+  {noreply, State};
+handle_info({gun_ws, ConnPid, _StreamnRef, {text, Message}}
+            ,#{conn_pid := ConnPid, callback := Callback, cstate := CState} = State) ->
+  Info = maps:fold(
+           fun(K, V, M) ->
+               M#{binary_to_atom(K, latin1) => V}
+           end,
+           #{},
+           jiffy:decode(Message, [return_maps])
+          ),
+  lager:info("Response is ~p", [Info]),
+  case Callback:handle_info(Info, CState) of
+    {ok, CStateU} ->
+      {noreply, State#{cstate => CStateU}};
+    {stop, Reason, CStateU} ->
+      {stop, Reason, State#{cstate => CStateU}}
+  end;
+handle_info({gun_down, ConnPid, Protocol, Reason, KilledStreams}
+           ,#{conn_pid := ConnPid} = State) ->
+  lager:info("Client connection is down ~p", [{ConnPid, Protocol, Reason, KilledStreams}]),
+  gun:close(ConnPid),
+  {stop, sock_closed, State};
+handle_info(Info, State) ->
+  lager:debug("Unknown message ~p when ~p", [Info, State]),
   {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-  ok.
+terminate(Reason, #{cstate := CState, callback := Callback} = State) ->
+  lager:info("Bifrost sock client behaviour terminated with ~p while in ~p", [Reason, State]),
+  case erlang:function_exported(Callback, terminate, 2) of
+    true -> Callback:terminate(Reason, CState);
+    false -> ok
+  end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
-
-
 
